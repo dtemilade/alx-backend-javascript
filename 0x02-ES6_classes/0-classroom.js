dd174fff2963b5da73fca6c7fcf5ef58ab126b04
dtemilade <dtemilade@gmail.com>
2024-04-12 00:02:12 +0000

    updating commits

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -2,6 +2,10 @@
  * Implement a class named ClassRoom
  */
 export default class ClassRoom {
+	/**
+	 * Implement a new class
+	 * @param {Number} maxStudentsSize - Maximum number in class
+	 */
 	constructor(maxStudentsSize) {
 		this._maxStudentsSize = maxStudentsSize;
 	}
